ceb1b9f7d6b2521f1b3353d28f23695c1d577cd5
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-10-08 23:05:01 -0300

    Fixing Navbar buttons Fixed #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Login from './pages/Login';
 
 function App() {
     return (
         <div className="App">
-            {/* {sessionStorage.getItem('user') === null ? <Login /> : <><Navbar /> <main className='main-outlet'><Outlet/></main></>} */}
             <Navbar />
             <main className="main-outlet">
                 <Outlet />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { AiFillHome, AiFillFileAdd, } from "react-icons/ai";
 import { IoPersonCircle } from "react-icons/io5";
 
@@ -7,14 +7,19 @@ import "./Navbar.css";
 
 const Navbar = () => {
     const [ActiveLink, setActiveLink] = useState('');
+    const location = useLocation();
+
+    useEffect(() => {
+        setActiveLink(location.pathname.replace('/', ''));
+    }, [location.pathname.replace('/', '')])
 
     return (
         <aside className="navbar-container">
             <img src="logo-crm-white.png" alt="Logo CRM" className="navbar-logo"/>
             <nav className="navbar-nav">
-                <Link className={ActiveLink == 'home' ? 'navbar-link active' : 'navbar-link'} onClick={() => setActiveLink('home')} to='/home'><AiFillHome />Página inicial</Link>
-                <Link className={ActiveLink == 'newcrm' ? 'navbar-link active' : 'navbar-link'} onClick={() => setActiveLink('newcrm')} to='/newcrm'><AiFillFileAdd />Criar CRM</Link>
-                <Link className={ActiveLink == 'profile' ? 'navbar-link active' : 'navbar-link'} onClick={() => setActiveLink('profile')} to='/profile'><IoPersonCircle />Perfil</Link>
+                <Link className={ActiveLink == 'home' ? 'navbar-link active' : 'navbar-link'} to='/home'><AiFillHome />Página inicial</Link>
+                <Link className={ActiveLink == 'newcrm' ? 'navbar-link active' : 'navbar-link'} to='/newcrm'><AiFillFileAdd />Criar CRM</Link>
+                <Link className={ActiveLink == 'profile' ? 'navbar-link active' : 'navbar-link'} to='/profile'><IoPersonCircle />Perfil</Link>
             </nav>
         </aside>
     );
