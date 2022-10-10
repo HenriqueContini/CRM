7e2fa64ab96990a9bd39f2dd6913ac4eef1e002f
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-10-10 10:33:52 -0300

    Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,22 @@
+import { useForm } from 'react-hook-form';
 import './styles/Profile.css';
 
 const Profile = () => {
+    const {register, handleSubmit} = useForm();
+
     return (
-        <h1>Profile</h1>
+        <section className='profile-container'>
+            <h1 className='profile-title'>Perfil</h1>
+            <h2 className='profile-name'>Henrique Contini</h2>
+            <form className='profile-form'>
+                <label htmlFor="profile-email">E-mail:</label>
+                <input id='profile-email' type="email" {...register('profile-email')} placeholder="E-mail" value={'henrique@gmail.com'}/>
+                <label htmlFor="profile-password">Senha:</label>
+                <input id='profile-password' type="password" {...register('profile-password')} placeholder="Senha"/>
+
+                <button type="submit" className='profile-button'>Salvar informações</button>
+            </form>
+        </section>
     );
 }
 
