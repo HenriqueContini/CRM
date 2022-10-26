6b00e8c39e381b14efb840658619c9dd592dc087
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-10-26 10:33:50 -0300

    Loading aware CRMs from API

--- src/components/AwareCRM.jsx
+++ src/components/AwareCRM.jsx
@@ -1,8 +1,26 @@
 import CardCRM from './CardCRM';
 import { BiSearchAlt2 } from 'react-icons/bi'
 import './styles/AwareCRM.css';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const AwareCRM = () => {
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    const navigate = useNavigate();
+    const [CRMs, setCRMs] = useState([]);
+
+    useEffect(() => {
+        if (user) {
+            let urlAPI = `http://localhost:8080/crm/awarecrms/${user.matricula}`;
+
+            fetch(urlAPI)
+                .then(r => r.json())
+                .then(json => setCRMs(json))
+        } else {
+            navigate('/')
+        }
+    }, [])
+
     return (
         <section className='awarecrm-section'>
             <form className='awarecrm-form'>
@@ -11,8 +29,9 @@ const AwareCRM = () => {
             </form>
 
             <div className='awarecrm-cards'>
-                <CardCRM />
-                <CardCRM />
+                {CRMs != undefined && CRMs.map(crm => (
+                    <CardCRM key={crm.id} crm_name={crm.nome_crm} author={crm.requerente} description={crm.descricao} date={crm.data_criacao} />
+                ))}
             </div>
         </section>
     )
