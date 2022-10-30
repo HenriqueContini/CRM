83423424c7f47c62ae3d4c6ff98907b1a6c9c29d
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-10-30 10:57:13 -0300

    CRM Dynamic Information

--- src/pages/Crm.jsx
+++ src/pages/Crm.jsx
@@ -1,14 +1,14 @@
 import CrmInfo from '../components/CrmInfo';
-import {AiFillFilePdf, AiFillFileWord, AiFillCloseCircle, AiFillCheckCircle, AiFillClockCircle} from 'react-icons/ai';
+import { AiFillFilePdf, AiFillFileWord, AiFillCloseCircle, AiFillCheckCircle, AiFillClockCircle } from 'react-icons/ai';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './styles/Crm.css';
 
 const Crm = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [CRM, setCRM] = useState({});
-    const [Departments, setDepartments] = useState({})
-    
+    const [Departments, setDepartments] = useState([])
+
     async function loadData() {
         try {
             let urlAPI = `http://localhost:8080/crm/getcrm/${id}`;
@@ -32,39 +32,39 @@ const Crm = () => {
             <section className="crm-wrapper">
                 <article className='crm-article'>
                     <h2 className='crm-article-title'>CRM</h2>
-                    <CrmInfo subtitle='Nome da CRM' info={CRM.nome_crm}/>
-                    <CrmInfo subtitle='Número da CRM' info={CRM.numero_crm}/>
-                    <CrmInfo subtitle='Versão' info={CRM.versao}/>
-                    <CrmInfo subtitle='Data de criação' info={`${new Date(CRM.data_criacao).getDate()}/${new Date(CRM.data_criacao).getMonth()}/${new Date(CRM.data_criacao).getFullYear()}`}/>
-                    <CrmInfo subtitle='Requerente' info={CRM.requerente}/>
-                    <CrmInfo subtitle='Setor' info={CRM.setor}/>
-                    <CrmInfo subtitle='Telefone ou ramal' info='19 9 99991234'/>
-                    <CrmInfo subtitle='E-mail:' info='henrique@gmail.com'/>
+                    <CrmInfo subtitle='Nome da CRM' info={CRM.nome_crm} />
+                    <CrmInfo subtitle='Número da CRM' info={CRM.numero_crm} />
+                    <CrmInfo subtitle='Versão' info={CRM.versao} />
+                    <CrmInfo subtitle='Data de criação' info={`${new Date(CRM.data_criacao).getDate()}/${new Date(CRM.data_criacao).getMonth()}/${new Date(CRM.data_criacao).getFullYear()}`} />
+                    <CrmInfo subtitle='Requerente' info={CRM.requerente} />
+                    <CrmInfo subtitle='Setor' info={CRM.setor} />
+                    <CrmInfo subtitle='Telefone ou ramal' info='19 9 99991234' />
+                    <CrmInfo subtitle='E-mail:' info='henrique@gmail.com' />
                 </article>
                 <article className='crm-article'>
                     <h2 className='crm-article-title'>Sobre</h2>
-                    <CrmInfo subtitle='A necessidade de:' info={CRM.necessidade}/>
-                    <CrmInfo subtitle='Impacto:' info={CRM.impacto}/>
+                    <CrmInfo subtitle='A necessidade de:' info={CRM.necessidade} />
+                    <CrmInfo subtitle='Impacto:' info={CRM.impacto} />
                 </article>
                 <article className='crm-article'>
                     <h2 className='crm-article-title'>Informações</h2>
-                    <CrmInfo subtitle='Descrição da demanda:' info={CRM.descricao}/>
-                    <CrmInfo subtitle='Objetivo a ser atendido:' info={CRM.objetivo}/>
-                    <CrmInfo subtitle='Justificativa:' info={CRM.justificativa}/>
-                    <CrmInfo subtitle='Alternativas:' info={CRM.alternativa}/>
-                    <CrmInfo subtitle='Sistemas envolvidos na mudança:' info={CRM.sistemas_envolvidos}/>
-                    <CrmInfo subtitle='Comportamento offline:' info={CRM.comportamento_offline}/>
-                    <CrmInfo subtitle='Esta CRM depende de outro desenvolvimento?' info={CRM.dependencia}/>
+                    <CrmInfo subtitle='Descrição da demanda:' info={CRM.descricao} />
+                    <CrmInfo subtitle='Objetivo a ser atendido:' info={CRM.objetivo} />
+                    <CrmInfo subtitle='Justificativa:' info={CRM.justificativa} />
+                    <CrmInfo subtitle='Alternativas:' info={CRM.alternativa} />
+                    <CrmInfo subtitle='Sistemas envolvidos na mudança:' info={CRM.sistemas_envolvidos} />
+                    <CrmInfo subtitle='Comportamento offline:' info={CRM.comportamento_offline} />
+                    <CrmInfo subtitle='Esta CRM depende de outro desenvolvimento?' info={CRM.dependencia} />
                 </article>
 
                 <article className="crm-article">
                     <h2 className="crm-article-title">Arquivos</h2>
                     <div className='crm-article-file'>
-                        <AiFillFilePdf className='crm-article-fileImage'/>
+                        <AiFillFilePdf className='crm-article-fileImage' />
                         <p className="crm-article-fileName">ArquivoComplementar.pdf</p>
                     </div>
                     <div className='crm-article-file'>
-                        <AiFillFileWord className='crm-article-fileImage'/>
+                        <AiFillFileWord className='crm-article-fileImage' />
                         <p className="crm-article-fileName">TextoComplementar.docx</p>
                     </div>
                 </article>
@@ -72,22 +72,15 @@ const Crm = () => {
                 <article className="crm-article">
                     <h2 className="crm-article-title">Setores envolvidos</h2>
                     <div className="crm-aware-list">
-                        {Departments.map(deparment => ('Continuação'))}
-                        <div className="crm-aware-item">
-                            <p className="crm-aware-department">Controladoria</p>
-                            <p className="crm-aware-user">Fulano</p>
-                            <AiFillCheckCircle className='crm-aware-img aware-accepted'/>
-                        </div>
-                        <div className="crm-aware-item">
-                            <p className="crm-aware-department">Fiscal</p>
-                            <p className="crm-aware-user"></p>
-                            <AiFillClockCircle className='crm-aware-img aware-pending'/>
-                        </div>
-                        <div className="crm-aware-item">
-                            <p className="crm-aware-department">Outro setor</p>
-                            <p className="crm-aware-user">Outro fulano</p>
-                            <AiFillCloseCircle className='crm-aware-img aware-rejected'/>
-                        </div>
+                        {Departments.map(d => (
+                            <div className="crm-aware-item" key={d.id_aprovacao}>
+                                <p className="crm-aware-department">{d.setor}</p>
+                                <p className="crm-aware-user">{d.responsavel}</p>
+                                {d.decisao === 'Pendente' ? <AiFillClockCircle className='crm-aware-img aware-pending'/> : null}
+                                {d.decisao === 'Aceitado' ? <AiFillCheckCircle className='crm-aware-img aware-accepted'/>: null}
+                                {d.decisao === 'Rejeitado' ? <AiFillCloseCircle className='crm-aware-img aware-rejected'/> : null}
+                            </div>
+                        ))}
                     </div>
                 </article>
             </section>
