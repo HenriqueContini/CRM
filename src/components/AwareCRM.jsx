55334717479178c9ceafbbaa54f6504c928928ed
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2022-11-02 13:34:41 -0300

    Search for: User CRM and Aware CRM

--- src/components/AwareCRM.jsx
+++ src/components/AwareCRM.jsx
@@ -1,13 +1,14 @@
 import CardCRM from './CardCRM';
 import { BiSearchAlt2 } from 'react-icons/bi'
-import './styles/AwareCRM.css';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import './styles/AwareCRM.css';
 
 const AwareCRM = () => {
     let user = JSON.parse(sessionStorage.getItem('user'));
     const navigate = useNavigate();
     const [CRMs, setCRMs] = useState([]);
+    const [FilteredCRMs, setFilteredCRMs] = useState([]);
 
     useEffect(() => {
         if (user) {
@@ -21,15 +22,22 @@ const AwareCRM = () => {
         }
     }, [])
 
+    useEffect(() => {
+        setFilteredCRMs(CRMs)
+    }, [CRMs])
+
+    function handleSearch(value) {
+        setFilteredCRMs(CRMs.filter((e) => e.nome_crm.toUpperCase().includes(value.toUpperCase())));
+    }
+
     return (
         <section className='awarecrm-section'>
             <form className='awarecrm-form'>
-                <input className='awarecrm-form-input' type="text" placeholder='Busque pelo nome de uma CRM' />
-                <button className='awarecrm-form-button' type='submit'><BiSearchAlt2 /></button>
+                <input className='awarecrm-form-input' type="search" placeholder='Busque pelo nome de uma CRM' onChange={(e) => handleSearch(e.target.value)}/>
             </form>
 
             <div className='awarecrm-cards'>
-                {CRMs != undefined && CRMs.map(crm => (
+                {FilteredCRMs.map(crm => (
                     <CardCRM key={crm.id} crm_id={crm.id} crm_name={crm.nome_crm} author={crm.requerente} description={crm.descricao} date={crm.data_criacao} />
                 ))}
             </div>
--- src/components/UserCRM.jsx
+++ src/components/UserCRM.jsx
@@ -1,5 +1,4 @@
 import CardCRM from './CardCRM';
-import { BiSearchAlt2 } from 'react-icons/bi';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './styles/UserCRM.css';
@@ -8,6 +7,7 @@ const UserCRM = () => {
     let user = JSON.parse(sessionStorage.getItem('user'));
     const navigate = useNavigate();
     const [CRMs, setCRMs] = useState([]);
+    const [FilteredCRMs, setFilteredCRMs] = useState([]);
     
     useEffect(() => {
         if(user) {
@@ -21,15 +21,22 @@ const UserCRM = () => {
         }
     }, [])
 
+    useEffect(() => {
+        setFilteredCRMs(CRMs)
+    }, [CRMs])
+
+    function handleSearch(value) {
+        setFilteredCRMs(CRMs.filter((e) => e.nome_crm.toUpperCase().includes(value.toUpperCase())));
+    }
+
     return (
         <section className='usercrm-section'>
             <form className='usercrm-form'>
-                <input className='usercrm-form-input' type="text" placeholder='Busque pelo nome de uma CRM' />
-                <button className='usercrm-form-button' type='submit'><BiSearchAlt2 /></button>
+                <input className='usercrm-form-input' type="search" placeholder='Busque pelo nome de uma CRM' onChange={(e) => handleSearch(e.target.value)}/>
             </form>
 
             <div className='usercrm-cards'>
-                {CRMs != undefined && CRMs.map(crm => (
+                {FilteredCRMs.map(crm => (
                     <CardCRM key={crm.id} crm_id={crm.id} crm_name={crm.nome_crm} author={crm.requerente} description={crm.descricao} date={crm.data_criacao}/>
                 ))}
             </div>
